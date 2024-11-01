eaea9a5a577efb1cd58314411529faddebf9c0be
leeden1119 <eunkyeonghlee@gmail.com>
2024-11-01 11:25:08 +0900

    Add New File

---
+++ generate_sudoku.py
@@ -0,0 +1,21 @@
+import random
+
+def generate_sudoku():
+    puzzle = []
+    
+    numbers = [0] * 9 + list(range(1,10))
+    
+    for _ in range(9): # 행 9개
+        row = []
+        for _ in range(9): # 열 9개
+            
+            # rand_int = random.randint(0,9)
+            rand_int = random.choice(numbers)
+            
+            row.append(rand_int)
+        puzzle.append(row)
+            
+    return puzzle
+
+sudoku_puzzle = generate_sudoku()
+print_board(sudoku_puzzle)
